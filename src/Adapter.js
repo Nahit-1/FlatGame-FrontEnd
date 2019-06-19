fabc39ece6f0b43c50a73b9d7f150f38f2a8b030
Nahit-1 <nahit86@outlook.com>
2019-06-19 16:19:03 +0100

    stable

--- src/Adapter.js
+++ src/Adapter.js
@@ -9,7 +9,7 @@ class Adapter {
     return fetch('http://localhost:3001/feedbacks', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ content: content, game_id: game_id, rating: 5, user_id: 4 })
+      body: JSON.stringify({ content: content, game_id: game_id, rating: 5, user_id: 64 })
     }).then(resp => resp.json())
   }
 
